5b1a935af26c20d4bf21912531178cb92d730ec6
Himanshu Vishwakarma <147297599+himanshuvkm12@users.noreply.github.com>
2025-07-12 15:48:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Room from './Pages/Room'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:roomID" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
